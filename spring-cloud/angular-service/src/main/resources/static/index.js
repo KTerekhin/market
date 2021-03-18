10a48b824170a49ef249224eda11c829d7c3c88c
Konstantin <davidblain53@gmail.com>
2021-03-18 21:06:42 +0300

    Исправлен файл application.yaml в модуле zuul-proxy

--- spring-cloud/angular-service/src/main/resources/static/index.js
+++ spring-cloud/angular-service/src/main/resources/static/index.js
@@ -16,20 +16,10 @@
 angular.module('app').controller('indexController', function ($scope, $http) {
     const contextPath = 'http://localhost:5555/cloud';
 
-    const headerDict = {
-        'Content-Type': 'Access-Control-Allow-Origin',
-        'Accept': 'application/json',
-        'Access-Control-Allow-Headers': 'Content-Type',
-    }
-    const requestOptions = {
-        headers: new Headers(headerDict),
-    };
-
     $scope.fillTable = function () {
         console.log("1");
         $http({
             url: contextPath + '/api/v1/products',
-            // url: 'http://localhost:51200/',
             method: 'GET',
         }).then(function (response) {
             $scope.ProductList = response.data;
